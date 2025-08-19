a5c1e71b1ec191c279c08c7fd07a836d4e526abf
Evan Sosenko <evan@getseam.com>
2025-08-19 14:25:49 -0700

    fix: Rename eslint entrypoint to eslint-config

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   tsconfig: 'tsconfig.build.json',
-  entry: ['src/index.ts', 'src/eslint.ts'],
+  entry: ['src/index.ts', 'src/eslint-config.ts'],
   format: ['cjs'],
   treeshake: true,
   dts: true,
