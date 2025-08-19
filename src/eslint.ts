840854b27cf6b0fa6d0238af90816ddf6fc2c27d
Evan Sosenko <evan@getseam.com>
2025-08-19 14:01:06 -0700

    fix: Update eslint config to v9

--- src/eslint.ts
+++ src/eslint.ts
@@ -1,61 +1,72 @@
-export default {
-  plugins: ['simple-import-sort', 'unused-imports'],
-  parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 'latest',
-  },
-  rules: {
-    'no-console': 'error',
-    'unused-imports/no-unused-imports': 'error',
-    'unused-imports/no-unused-vars': [
-      'error',
-      {
-        vars: 'all',
-        varsIgnorePattern: '^_',
-        args: 'after-used',
-        argsIgnorePattern: '^_',
-        ignoreRestSiblings: true,
-      },
-    ],
-    'import/extensions': ['error', 'ignorePackages'],
-    'import/no-duplicates': ['error', { 'prefer-inline': true }],
-    'import/no-relative-parent-imports': 'error',
-    'simple-import-sort/imports': [
-      'error',
-      {
-        groups: [
-          ['^\\u0000'],
-          ['^node:'],
-          ['^@?\\w'],
-          ['^lib/'],
-          ['^'],
-          ['^\\.'],
-        ],
-      },
-    ],
-    'simple-import-sort/exports': 'error',
-  },
-  overrides: [
-    {
-      files: ['*.js', '*.mjs', '*.cjs'],
-      extends: ['standard', 'prettier'],
+import { globalIgnores } from 'eslint/config'
+import importPlugin from 'eslint-plugin-import'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import unusedImports from 'eslint-plugin-unused-imports'
+import neostandard, { resolveIgnoresFromGitignore } from 'neostandard'
+
+const files = ['**/*.{ts,tsx}']
+
+export default [
+  globalIgnores(resolveIgnoresFromGitignore()),
+  ...neostandard({ ts: true, noStyle: true }),
+  {
+    files,
+    rules: {
+      'no-console': 'error',
+      '@typescript-eslint/no-non-null-assertion': 'error',
     },
-    {
-      files: ['*.ts', '*.tsx'],
-      extends: ['standard-with-typescript', 'prettier'],
-      parserOptions: {
-        project: './tsconfig.json',
-      },
-      rules: {
-        '@typescript-eslint/no-import-type-side-effects': 'error',
-        '@typescript-eslint/consistent-type-imports': [
-          'error',
-          {
-            fixStyle: 'inline-type-imports',
-          },
-        ],
-        '@typescript-eslint/no-unused-vars': 'off',
-      },
+  },
+  {
+    files,
+    plugins: {
+      'unused-imports': unusedImports,
+      import: importPlugin,
     },
-  ],
-}
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-import-type-side-effects': 'error',
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          fixStyle: 'inline-type-imports',
+        },
+      ],
+      'import/extensions': ['error', 'ignorePackages'],
+      'import/no-duplicates': ['error', { 'prefer-inline': true }],
+      'import/no-relative-parent-imports': 'error',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'error',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
+  {
+    files,
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            ['^\\u0000'],
+            ['^node:'],
+            ['^@?\\w'],
+            ['^lib/'],
+            ['^'],
+            ['^\\.'],
+          ],
+        },
+      ],
+      'simple-import-sort/exports': 'error',
+    },
+  },
+]
